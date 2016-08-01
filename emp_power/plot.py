4e3d90f8d67d6e523a55a288616eda2ceb0e4c7a
J W Debelius <j.debelius@gmail.com>
2016-08-01 09:13:46 -0700

    Some nice doc string for plotting

--- emp_power/plot.py
+++ emp_power/plot.py
@@ -1,34 +1,47 @@
-# from matplotlib import use, rcParams
-# use('Agg') #noqa
-import matplotlib.pyplot as plt
+# from matplotlib import use
+use('Agg') #noqa
 import numpy as np
 import scipy
 import seaborn as sn
 import statsmodels.formula.api as smf
 
-# rcParams['font.family'] = 'sans-serif'
-# rcParams['font.sans-serif'] = ['Helvetica', 'Arial']
-# rcParams['text.usetex'] = True
-
-# sn.set_style()
-
-# possible_markers = ['o', 's', '*', '^', 'd', 'x']
-# marker_sizes = [10, 10, 13, 10, 10, 10]
-# possible_colors = sn.color_palette('Set1', n_colors=8)
-
 
 def gradient_regression(ax, x, y, gradient, data, size=40,
                         alpha=0.5, line_color='k', **reg_kwargs):
-    """Some nice doc string"""
+    """Creates a regression plot colored by gradient metadata
+
+    Parameters
+    ----------
+    ax : Axes
+        The axis where the data should be plotted
+    x, y, gradient: str
+        The names of the columns in `data` which contain the predictor,
+        response, and gradient variables respectively to be used in the
+        regression plot.
+    data : DataFrame
+        A pandas dataframe containing columns `x`, `y`, and `gradient`.
+    size : int, optional
+        The size of the points on the plot
+    alpha : float, optional
+        The transparency of the plots
+    line_color : string; tuple, optional
+        The color regression line.
+
+    Also See
+    --------
+        matplotlib.pyplot.scatter
+        seaborn.regplot
+        scipy.stats.linregress
+    """
 
     data.dropna(subset=[x, y, gradient], inplace=True)
     scatter_kws = {
-                   'c': data[gradient], # Sets the color gradient
-                   's': size, # sets the size to 40 points.
-                   'alpha': alpha, # Makes the points partially transparent
-                   'edgecolors': 'None' # hides the edges
+                   'c': data[gradient],
+                   's': size,
+                   'alpha': alpha,
+                   'edgecolors': 'None'
                    }
-    line_kws = {'color': line_color # Sets the regression line color
+    line_kws = {'color': line_color
                 }
 
     # Plots the regression plot
@@ -42,17 +55,44 @@ def gradient_regression(ax, x, y, gradient, data, size=40,
     ax.text(0.95, 0.05, 'm = %1.2f\nr2 = %1.3f' % (m, r), ha='right')
 
 
-def gradient_residuals(ax, x, y, gradient, data, x_resid=None, cmap='Spectral',
+def gradient_residuals(ax, x, y, gradient, data, x_resid=None,
                        size=40, alpha=0.5, line_kws=None):
-    """..."""
+    """Creates a colored plot of the regression residuals against a category
+
+    Parameters
+    ----------
+    ax : Axes
+        The axis where the data should be plotted
+    x, y, gradient: str
+        The names of the columns in `data` which contain the predictor,
+        response, and gradient variables respectively to be used in the
+        regression plot.
+    data : DataFrame
+        A pandas dataframe containing columns `x`, `y`, and `gradient`.
+    x_resid : str, optional
+        The name of the column to be used to plot compared to the residual.
+        If nothing is specifed, the `x` variable will be used.
+    size : int, optional
+        The size of the points on the plot
+    alpha : float, optional
+        The transparency of the plots
+    line_color : string; tuple, optional
+        The color regression line.
+
+    Also See
+    --------
+        matplotlib.pyplot.scatter
+        seaborn.residplot
+        statsmodels.regression.linear_model.OLS
+    """
+
     if x_resid is None:
         x_resid = x
     scatter_kws = {
-                   # 'c': data[gradient], # Sets the color gradient
-                   's': size, # sets the size to 40 points.
-                   'cmap': cmap, # Sets the colormap as specified
-                   'alpha': alpha, # Makes the points partially transparent
-                   'edgecolors': 'None' # hides the edges
+                   'c': data[gradient],
+                   's': size,
+                   'alpha': alpha,
+                   'edgecolors': 'None'
                     }
     if line_kws is None:
         line_kws = {'color': 'k',
@@ -69,7 +109,17 @@ def gradient_residuals(ax, x, y, gradient, data, x_resid=None, cmap='Spectral',
 
 
 def format_regression_axis(ax):
-    """Makes a clean, pretty axis"""
+    """Makes a clean, pretty axis for the regression plots
+
+    This sets axis limits at 0, 1, with ticks at 0.25, and removes all axis
+    labels. The method is primarily used for the power regression plots.
+
+    Parameters
+    ----------
+    ax : Axis
+        The axis to be formatted
+
+    """
     ax.set_xticks(np.arange(0, 1.1, 0.25))
     ax.set_yticks(np.arange(0, 1.1, 0.25))
     ax.set_xticklabels('')
@@ -81,6 +131,24 @@ def format_regression_axis(ax):
 
 
 def format_residual_ax(ax, xlim=None, ylim=None, num_ticks=5):
+    """Creates a clean axis for the residual plots
+
+    This method is primarily designed to make neatly formatted axes for
+    residual axes in a plot.
+
+    Parameters
+    ----------
+    ax : Axis
+        The axis to be formatted
+    xlim, ylim : list, optional
+        The x and y limits for the residual axis. By default, the current x
+        limits will be used.
+    ylim : list, optional
+        The y limits for the residual axis. By default, these will be set as
+        symetrical values at the limits.
+    num_ticks : int, optional
+        The number of ticks ot use on the x and y axis.
+    """
     if xlim is None:
         xlim = ax.get_xlim()
     if ylim is None:
@@ -100,6 +168,7 @@ def format_residual_ax(ax, xlim=None, ylim=None, num_ticks=5):
 
 
 def _set_ticks(lims, num_ticks):
+    """Sets the ticks limits on a plot"""
     range_ = lims[1] - lims[0]
     interval = range_ / num_ticks
     ticks = np.arange(lims[0], lims[1] + interval / 2, interval)
@@ -107,6 +176,7 @@ def _set_ticks(lims, num_ticks):
 
 
 def _get_symetrical(lims):
+    """Calculates symetrical limits"""
     lim_max = max(lims)
     lims = [-lim_max, lim_max]
     return lims
