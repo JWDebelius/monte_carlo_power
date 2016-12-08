82c3362b0b35250040cceeadeac8f73542b7bbc0
J W Debelius <j.debelius@gmail.com>
2016-12-08 11:44:22 -0800

    adjust effects to account for filtering and a one-way distribution

--- machivellian/effects.py
+++ machivellian/effects.py
@@ -10,7 +10,8 @@ import numpy as np
 from scipy.stats import norm as z
 
 
-def z_effect(counts, power, alpha=0.05):
+def z_effect(counts, power, alpha=0.05, lower_lim=0.1, upper_lim=0.95,
+    size_lim=0):
     """Estimates the effect size for power based on the z distribution
 
     This is based on the equations in [1]_, the equation assumes a positive
@@ -29,6 +30,13 @@ def z_effect(counts, power, alpha=0.05):
         the sampling depths in counts.
     alpha : float, optional
         The critial value used to calculate the power
+    lower_lim, upper_lim: float, optional
+        The smallest and largest power values to consider fitting.
+        This is selected to account for the aspmytotic behavior of cumualtive
+        distribution functions.
+    size_lim : int, optional
+        The minimum number of observations in the test required to estimate
+        an effect size. By default, no limit is set.
 
     Returns
     -------
@@ -53,7 +61,9 @@ def z_effect(counts, power, alpha=0.05):
     z_diff = z.ppf(power) + z.ppf(1 - alpha)
     eff = np.sqrt(np.square(z_diff) / counts)
 
-    eff[power == 1] = np.nan
+    eff[power < lower_lim] = np.nan
+    eff[power > upper_lim] = np.nan
+    eff[counts < size_lim] = np.nan
     eff[np.isinf(eff)] = np.nan
 
     return eff
@@ -95,8 +105,14 @@ def z_power(counts, effect, alpha=0.05):
 
 def _check_shapes(counts, power):
     """Checks that counts and power have the same shape"""
-    counts = np.asarray(counts)
-    power = np.asarray(power)
+    if isinstance(counts, (float, int)):
+        counts = np.array([counts])
+    else:
+        counts = np.asarray(counts)
+    if isinstance(power, (float, int)):
+        power = np.array([power])
+    else:
+        power = np.asarray(power)
     c_shape = counts.shape
     p_shape = power.shape
 
@@ -112,3 +128,4 @@ def _check_shapes(counts, power):
     else:
         raise ValueError('there must be a power value for every resample '
                          'depth')
+
