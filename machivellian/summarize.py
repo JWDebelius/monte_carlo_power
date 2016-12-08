6aa87cff28e83594ce59f2b53e6ca734889a60f3
J W Debelius <j.debelius@gmail.com>
2016-12-08 11:43:01 -0800

    working update of summarize

--- machivellian/summarize.py
+++ machivellian/summarize.py
@@ -15,8 +15,7 @@ import pandas as pd
 import machivellian.effects as eff
 
 
-def summarize_power(power_summary, sim_num, test, colors, dists=None,
-    num_groups=2):
+def summarize_power(power_summary, sim_num, test, colors):
     """Generates a dataframe describing a power calculation run
 
     Please note, this function is primarily designed for use with the
@@ -34,11 +33,6 @@ def summarize_power(power_summary, sim_num, test, colors, dists=None,
     colors : dictionary
         A dictionary linking the counts to the colors to be used in regression
         and residual gradient plots
-    dists : list, optional
-        The list of distributions to be used to calculate effect sizes and
-        power. By default, f, t, and z distributions will be used.
-    num_groups : int, optional
-        The number of groups included, to be used with the F distribution
 
     Returns
     -------
@@ -47,30 +41,22 @@ def summarize_power(power_summary, sim_num, test, colors, dists=None,
         single observation. Columns include `counts`, `traditional`
         (distribution-based power), `empirical` (empirically based power),
         `sim_position` (a unique identifier for each simulation), `test`,
-        `alpha` (the critical value), `sim_num` (the simulation id), `colors`,
-        and the corresponding `effect` and `power` for each distribution
-        specified in `dists`
+        `alpha` (the critical value), `sim_num` (the simulation id), and
+        `colors`,
     """
-
-    dists = _get_dists(dists)
 
     # Builds the initia data frame
     run_summary = _build_summary_frame(power_summary)
 
     #  Adds static information
     run_summary['test'] = test
-    run_summary['alpha'] = power_summary['alpha']
+    run_summary['alpha'] = power_summary['alpha'] * power_summary['alpha_adj']
     run_summary['sim_num'] = sim_num
     run_summary['p_all'] = power_summary['original_p']
+    run_summary['statistic'] = power_summary['statistic']
 
     # Includes the count values to be plotted
     run_summary['colors'] = run_summary['counts'].apply(lambda x: colors[x])
-
-    # Calculates the effect sizes
-    _calculate_effect_size(run_summary, dists, num_groups)
-
-    # # Calculates the mean effects and the power
-    _calculate_power(run_summary, dists, num_groups)
 
     run_summary['index'] = (
         run_summary['test'] + '.' +
@@ -82,86 +68,52 @@ def summarize_power(power_summary, sim_num, test, colors, dists=None,
     return run_summary
 
 
-def modify_effect_size(df, drop_index, dists=None, num_groups=2):
-    """Calculates a modified effect size based on dropped values
+# def modify_effect_size(df, drop_index, dists=None, num_groups=2):
+#     """Calculates a modified effect size based on dropped values
 
-    Parameters
-    ----------
-    df : DataFrame
-        The output of `summarize_power` or a concatenated description of the
-        output of summarize power
-    drop_index : list, ndarray or Index
-        The observations to be excluded from the calculations
-    dists : list
-        The list of distributions to be used to calculate effect sizes and
-        power.
-    num_groups : int, optional
-        The number of groups included, to be used with the F distribution
+#     Parameters
+#     ----------
+#     df : DataFrame
+#         The output of `summarize_power` or a concatenated description of the
+#         output of summarize power
+#     drop_index : list, ndarray or Index
+#         The observations to be excluded from the calculations
+#     dists : list
+#         The list of distributions to be used to calculate effect sizes and
+#         power.
+#     num_groups : int, optional
+#         The number of groups included, to be used with the F distribution
 
-    Returns
-    -------
-    DataFrame
-        Summarizes the recalculated effect sizes and power. Columns include
-        `counts`, `traditional` (distribution-based power),
-        `empirical` (empirically based power), `sim_position` (a unique
-        idenifier for each simulation), `test`, `alpha` (the critical value),
-        `sim_num` (the simulation id), `colors`, and the corresponding
-        `effect` and `power` for each distribution specified in `dists.`
-    """
-    copy_cols = ['empirical', 'traditional', 'counts', 'color', 'simulation',
-                 'sim_pos', 'test', 'alpha']
-    copy_cols.extend(['%s_effect' % d for d in dists])
-    df_mod = copy.deepcopy(df)
-    df_mod.loc[drop_index, ['%s_effect' % d for d in dists]] = np.nan
-    _calculate_power(df_mod, dists, num_groups)
+#     Returns
+#     -------
+#     DataFrame
+#         Summarizes the recalculated effect sizes and power. Columns include
+#         `counts`, `traditional` (distribution-based power),
+#         `empirical` (empirically based power), `sim_position` (a unique
+#         idenifier for each simulation), `test`, `alpha` (the critical value),
+#         `sim_num` (the simulation id), `colors`, and the corresponding
+#         `effect` and `power` for each distribution specified in `dists.`
+#     """
+#     copy_cols = ['empirical', 'traditional', 'counts', 'color', 'simulation',
+#                  'sim_pos', 'test', 'alpha']
+#     copy_cols.extend(['%s_effect' % d for d in dists])
+#     df_mod = copy.deepcopy(df)
+#     df_mod.loc[drop_index, ['%s_effect' % d for d in dists]] = np.nan
+#     _calculate_power(df_mod, dists, num_groups)
 
-    return df_mod
-
-
-def calc_f_effect(x, col2='emperical', num_groups=2):
-    """Wraps the f-based power calculation for pandas `apply`"""
-    effect = eff.f_effect([x['counts']],
-                          [x[col2]],
-                          alpha=x['alpha'],
-                          groups=num_groups)[0][0]
-    return effect
+#     return df_mod
 
 
-def calc_t_effect(x, col2='emperical', num_groups=2):
-    """Wraps the t-based power calculation for pandas `apply`"""
-    effect = eff.t_effect([x['counts']],
-                          [x[col2]],
-                          alpha=x['alpha'])[0][0]
-    return effect
-
-
-def calc_z_effect(x, col2='emperical', num_groups=2):
+def calc_z_effect(x, col2='empirical', num_groups=2):
     """Wraps the z-based power calculation for pandas `apply`"""
     effect = eff.z_effect([x['counts']],
                           [x[col2]],
-                          alpha=x['alpha'])[0][0]
+                          alpha=x['alpha'])[0]
     return effect
 
 
-def calc_f_power(x, col2, num_groups=2):
-    """Wraps the f-based power calulation"""
-    power = eff.f_power(counts=x['counts'],
-                        effect=x[col2],
-                        alpha=x['alpha'],
-                        groups=num_groups)
-    return power
-
-
-def calc_t_power(x, col2, num_groups=2):
-    """Wraps the f-based power calulation"""
-    power = eff.t_power(counts=x['counts'],
-                        effect=x[col2],
-                        alpha=x['alpha'])
-    return power
-
-
 def calc_z_power(x, col2, num_groups=2):
-    """Wraps the f-based power calulation"""
+    """Wraps the z-based power calulation"""
     power = eff.z_power(counts=x['counts'],
                         effect=x[col2],
                         alpha=x['alpha'])
@@ -186,8 +138,9 @@ def _build_summary_frame(sim):
     # Sets up the summary dictionary
     run_summary = pd.DataFrame.from_dict(
         {'counts': np.hstack(np.array([counts] * empr_r)),
-         'emperical': np.hstack(empirical),
-         'traditional': np.hstack(np.array([traditional] * empr_r)),
+         'empirical': np.hstack(empirical),
+         'traditional':
+            np.hstack(np.array([traditional] * empr_r)),
          'sim_position': np.hstack([np.arange(empr_c) + i * 10
                                     for i in np.arange(empr_r)]),
          })
@@ -195,50 +148,35 @@ def _build_summary_frame(sim):
     return run_summary
 
 
-def _calculate_effect_size(df, distributions, num_groups=2):
-    """Adds the effect sizes to the dataframe"""
-    for dist in distributions:
-        if dist == 'f2':
-            ng = 2
-        else:
-            ng = num_groups
-        f_ = partial(effect_lookup[dist], col2='emperical',
-                     num_groups=ng)
-        df['%s_effect' % dist] = df.apply(f_, axis=1)
+# def _calculate_effect_size(df, distributions, num_groups=2):
+#     """Adds the effect sizes to the dataframe"""
+#     for dist in distributions:
+#         if dist == 'f2':
+#             ng = 2
+#         else:
+#             ng = num_groups
+#         f_ = partial(effect_lookup[dist], col2='empirical',
+#                      num_groups=ng)
+#         df['%s_effect' % dist] = df.apply(f_, axis=1)
 
 
-def _calculate_power(df, distributions, num_groups=2):
-    """Adds power calculations to the dataframe"""
-    mean_lookup = (df.groupby('sim_num').mean()
-                   [['%s_effect' % d for d in distributions]].to_dict())
-    for dist in distributions:
-        df['%s_mean' % dist] = \
-            df['sim_num'].replace(mean_lookup['%s_effect' % dist])
-        if dist == 'f2':
-            ng = 2
-        else:
-            ng = num_groups
-        f_ = partial(power_lookup[dist],
-                     col2='%s_mean' % dist,
-                     num_groups=ng)
-        df['%s_power' % dist] = df.apply(f_, axis=1)
+# def _calculate_power(df, distributions, num_groups=2):
+#     """Adds power calculations to the dataframe"""
+#     mean_lookup = (df.groupby('sim_num').mean()
+#                    [['%s_effect' % d for d in distributions]])
+#     for d in distributions:
+#         mean_lookup.loc[mean_lookup['%s_effect' % d] < 0.1,
+#                         '%s_effect' % d] = np.nan
+#     mean_lookup = mean_lookup.to_dict()
 
-
-def _get_dists(dists):
-    # Pulls out the required information
-    if dists is None:
-        return ['f', 'f2', 't', 'z']
-    else:
-        return dists
-
-
-effect_lookup = {'f': calc_f_effect,
-                 'f2': calc_f_effect,
-                 't': calc_t_effect,
-                 'z': calc_z_effect
-                 }
-
-power_lookup = {'f': calc_f_power,
-                'f2': calc_f_power,
-                't': calc_t_power,
-                'z': calc_z_power}
+#     for dist in distributions:
+#         df['%s_mean' % dist] = \
+#             df['sim_num'].replace(mean_lookup['%s_effect' % dist])
+#         if dist == 'f2':
+#             ng = 2
+#         else:
+#             ng = num_groups
+#         f_ = partial(power_lookup[dist],
+#                      col2='%s_mean' % dist,
+#                      num_groups=ng)
+#         df['%s_power' % dist] = df.apply(f_, axis=1)
--- machivellian/tests/test_summarize.py
+++ machivellian/tests/test_summarize.py
@@ -8,16 +8,11 @@ import pandas as pd
 import pandas.util.testing as pdt
 
 from machivellian.summarize import (summarize_power,
-                                    calc_f_effect,
-                                    calc_t_effect,
                                     calc_z_effect,
-                                    calc_f_power,
-                                    calc_t_power,
                                     calc_z_power,
-                                    _build_summary_frame,
-                                    _calculate_effect_size,
+                                    build_summary_frame,
+                                    # _calculate_effect_size,
                                     modify_effect_size,
-                                    _get_dists,
                                     )
 
 
@@ -37,6 +32,7 @@ class SummarizeTest(TestCase):
             'num_obs': 100,
             'original_p': 0.001,
             'alpha': 0.05,
+            'statistic': 12,
             }
         self.early_summary = pd.DataFrame(
             data=np.array([[5, 15, 25, 35, 45, 55, 65, 75, 85, 95],
@@ -48,13 +44,13 @@ class SummarizeTest(TestCase):
                             0.41484527, 0.45459791],
                            np.arange(10),
                            ]),
-            index=['counts', 'emperical', 'traditional', 'sim_position']
+            index=['counts', 'empirical', 'traditional', 'sim_position']
             ).T
         self.early_summary['counts'] = \
             self.early_summary['counts'].apply(lambda x: int(x))
         self.early_summary['sim_position'] = \
             self.early_summary['sim_position'].apply(lambda x: int(x))
-        self.early_summary = self.early_summary[['counts', 'emperical',
+        self.early_summary = self.early_summary[['counts', 'empirical',
                                                  'sim_position',
                                                  'traditional']]
 
@@ -73,12 +69,10 @@ class SummarizeTest(TestCase):
                                sim_num=0,
                                test='test',
                                colors={i: 'k' % i
-                                       for i in np.arange(5, 100, 10)},
-                               dists=['z'])
-        columns = pd.Index(['counts', 'emperical', 'sim_position',
+                                       for i in np.arange(5, 100, 10)})
+        columns = pd.Index(['counts', 'empirical', 'sim_position',
                             'traditional', 'test', 'alpha',
-                            'sim_num', 'p_all', 'colors', 'z_effect',
-                            'z_mean', 'z_power'])
+                            'sim_num', 'p_all', 'statistic', 'colors'])
         index = pd.Index(['test.0.%i' % i for i in np.arange(0, 10)],
                          name='index')
         pdt.assert_index_equal(test.columns, columns)
@@ -87,35 +81,16 @@ class SummarizeTest(TestCase):
         self.assertEqual(test['alpha'].unique(), 0.05)
         self.assertEqual(test['sim_num'].unique(), 0)
         self.assertEqual(test['colors'].unique(), 'k')
+        self.assertEqual(test['statistic'].unique(), 12)
 
-    def test_modify_effect_sizes(self):
-        drop_index = ['B']
-        mod = modify_effect_size(self.df, drop_index, ['z'])
-        self.assertTrue(pd.isnull(mod.loc['B', 'z_effect']))
-
-    def test_calc_f_effect(self):
-        known = pd.Series([0.191889, 0.191941], index=['A', 'B'])
-        test = self.df.apply(calc_f_effect, axis=1)
-        pdt.assert_series_equal(known, test)
-
-    def test_calc_t_effect(self):
-        known = pd.Series([0.26728415,  0.26832483], index=['A', 'B'])
-        test = self.df.apply(calc_t_effect, axis=1)
-        pdt.assert_series_equal(known, test)
+    # def test_modify_effect_sizes(self):
+    #     drop_index = ['B']
+    #     mod = modify_effect_size(self.df, drop_index, ['z'])
+    #     self.assertTrue(pd.isnull(mod.loc['B', 'z_effect']))
 
     def test_calc_z_effect(self):
         known = pd.Series([0.18700873,  0.18797725], index=['A', 'B'])
         test = self.df.apply(calc_z_effect, axis=1)
-        pdt.assert_series_equal(known, test)
-
-    def test_calc_f_power(self):
-        known = pd.Series([0.81885655,  0.90630465], index=['A', 'B'])
-        test = self.df.apply(partial(calc_f_power, col2='z_effect'), axis=1)
-        pdt.assert_series_equal(known, test)
-
-    def test_calc_t_power(self):
-        known = pd.Series([0.54068791,  0.65018550], index=['A', 'B'])
-        test = self.df.apply(partial(calc_t_power, col2='z_effect'), axis=1)
         pdt.assert_series_equal(known, test)
 
     def test_calc_z_power(self):
@@ -124,30 +99,25 @@ class SummarizeTest(TestCase):
         pdt.assert_series_equal(known, test)
 
     def test_build_summary_frame(self):
-        test = _build_summary_frame(self.power_summary)
+        test = build_summary_frame(self.power_summary)
         pdt.assert_frame_equal(self.early_summary, test)
 
     def test_build_summary_frame_no_trad(self):
         self.power_summary['traditional'] = None
         known = pd.Series(np.ones(10,) * np.nan, name='traditional')
-        test = _build_summary_frame(self.power_summary)
+        test = build_summary_frame(self.power_summary)
         pdt.assert_series_equal(known, test['traditional'])
 
-    def test_calculate_effect_size(self):
-        known = copy.deepcopy(self.df)
-        known['z_effect'] = [0.18700873,  0.18797725]
-        _calculate_effect_size(self.df, ['z'])
-        pdt.assert_frame_equal(known, self.df)
+    # def test_calculate_effect_size(self):
+    #     known = copy.deepcopy(self.df)
+    #     known['z_effect'] = [0.18700873,  0.18797725]
+    #     _calculate_effect_size(self.df, ['z'])
+    #     pdt.assert_frame_equal(known, self.df)
 
-    def test_calculate_power(self):
-        known = copy.deepcopy(self.df)
-        known['z_power'] = [0.84087872,  0.91836203]
-        known['z_mean'] = 0.5
-
-    def test_get_dists(self):
-        known = ['f', 'f2', 't', 'z']
-        test = _get_dists(None)
-        self.assertEqual(known, test)
+    # def test_calculate_power(self):
+    #     known = copy.deepcopy(self.df)
+    #     known['z_power'] = [0.84087872,  0.91836203]
+    #     known['z_mean'] = 0.5
 
 if __name__ == '__main__':
     main()
